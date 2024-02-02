4abac2c170529ee94be6dbc9ca075186703d31aa
Ольга Лосева <olgalo@list.ru>
2024-02-02 18:55:45 +0300

    Добавьте файлы проекта.

---
+++ Lab8-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Метод выводит на консоль содержимое указанной папки и содержимое всех подкаталогов
+           
+                string path = @"D:\TEMP";
+                string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach (string f in files)
+                {
+                    Console.WriteLine(f);
+                }
+                Console.ReadKey();
+            }
+    }
+}
---
+++ Lab8-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8_2
+{
+    class Program
+    {
+        //Метод создает текстовый файл и записывает в него 10 случайных чисел. 
+        //Затем считывает их, расчитывает сумму чисел и выводит результат на консоль
+        static void Main(string[] args)
+        {
+            string path = @"D:\TEMP\Num1.txt";
+            const int n = 10;
+            Random rand = new Random();
+            using (StreamWriter sWriter = new StreamWriter(path, false))    //запись в файл n случайных чисел
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    sWriter.WriteLine(rand.Next(-100, 100));
+                }
+            }
+
+            int summ = 0;
+            using (StreamReader sReader = new StreamReader(path))           //чтение из файла n чисел и их суммирование
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    summ = summ + Convert.ToInt32(sReader.ReadLine());
+                }
+            }
+            Console.Write("Сумма {0} записанных чисел = {1}", n, summ);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
